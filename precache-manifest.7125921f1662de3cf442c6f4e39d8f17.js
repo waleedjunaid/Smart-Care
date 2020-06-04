69d586f79427fb681fa445b8a1f3b5aa737640d4
Muhammad Waleed <muhammad.waleed3@aku.edu>
2020-06-05 03:02:56 +0500

    Updates

---
+++ precache-manifest.7125921f1662de3cf442c6f4e39d8f17.js
@@ -0,0 +1,246 @@
+self.__precacheManifest = [
+  {
+    "revision": "1a3e62653c08254a5324",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/0.adc72aed.chunk.js"
+  },
+  {
+    "revision": "5676b4fa9f177f2bdd01",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/1.9301913f.chunk.js"
+  },
+  {
+    "revision": "371406fe70da0926ecae",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/2.58e13141.chunk.js"
+  },
+  {
+    "revision": "5174a0a42fd38fb39270",
+    "url": "https://waleedjunaid.io/Smart-Care/static/css/main.233d3351.chunk.css"
+  },
+  {
+    "revision": "5174a0a42fd38fb39270",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/main.799c3d7e.chunk.js"
+  },
+  {
+    "revision": "38c34b20e650fe3b9e43",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/runtime~main.55668061.js"
+  },
+  {
+    "revision": "7419be879ab550608b56",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/5.54b67ed0.chunk.js"
+  },
+  {
+    "revision": "1418aae248ab59855f4b",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/6.a0c1cd71.chunk.js"
+  },
+  {
+    "revision": "7e1d4987e49d57b84c22",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/7.250ce2e4.chunk.js"
+  },
+  {
+    "revision": "0317a0d03e914507bc51",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/8.efb6ff14.chunk.js"
+  },
+  {
+    "revision": "154b4befc314006cfd09",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/9.c543f2fc.chunk.js"
+  },
+  {
+    "revision": "11071b20cc67714ef74d",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/10.5969c9e8.chunk.js"
+  },
+  {
+    "revision": "edf12097f6a7c3db289b",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/11.e7451aa3.chunk.js"
+  },
+  {
+    "revision": "fcc7703e3bb7bc40c178",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/12.5af7b093.chunk.js"
+  },
+  {
+    "revision": "0ae97e722de474907254",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/13.a6f6ca99.chunk.js"
+  },
+  {
+    "revision": "b81e19bed3d064883a2f",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/14.f541120a.chunk.js"
+  },
+  {
+    "revision": "180bdffc89c1babfff69",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/15.16fc2b89.chunk.js"
+  },
+  {
+    "revision": "c1607d5fd7874d083886",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/16.3998bae8.chunk.js"
+  },
+  {
+    "revision": "7e4681d88cc9cd7d1609",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/17.480bfb86.chunk.js"
+  },
+  {
+    "revision": "b04627e9c353c97d974c",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/18.ed796d51.chunk.js"
+  },
+  {
+    "revision": "540a7d76b9eeb0aebb40",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/19.b0c23205.chunk.js"
+  },
+  {
+    "revision": "e97d90b5a796aabb533a",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/20.401706c2.chunk.js"
+  },
+  {
+    "revision": "7ce8c90bbd17b9b70cc9",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/21.f6817e27.chunk.js"
+  },
+  {
+    "revision": "78f927c0f56cb3e09ec7",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/22.79cc9388.chunk.js"
+  },
+  {
+    "revision": "99cb821765060594b1ee",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/23.38593104.chunk.js"
+  },
+  {
+    "revision": "6e7fc25b29fbcf717b94",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/24.0523a552.chunk.js"
+  },
+  {
+    "revision": "7eda3bacc7564fe23aa2",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/25.1cb0d082.chunk.js"
+  },
+  {
+    "revision": "6712e1e9e8608f2a69b1",
+    "url": "https://waleedjunaid.io/Smart-Care/static/js/26.8f455c5b.chunk.js"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "80d4a4e5af9b6cf3882e4a34c5497542",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/sidebar-4.80d4a4e5.jpg"
+  },
+  {
+    "revision": "1f7beca5bf4cda1803cd59cd881c372d",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_1.1f7beca5.jpg"
+  },
+  {
+    "revision": "82e7c41e073394f808fd382c97e71884",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_2.82e7c41e.jpg"
+  },
+  {
+    "revision": "6e25d86d33b97ab231889fd4a38530b4",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_3.6e25d86d.jpg"
+  },
+  {
+    "revision": "fd53372585bf0ef377e97ee9dd7ea925",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_5.fd533725.jpg"
+  },
+  {
+    "revision": "af595005f7c608b19134c0c411a065f6",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/waleedjunaid.af595005.jpg"
+  },
+  {
+    "revision": "c54eef50718560d1268aa8e92025972d",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/background_1920-2.c54eef50.jpg"
+  },
+  {
+    "revision": "b175e1c4790112fe609a0f3279e8535d",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/logo_200.b175e1c4.png"
+  },
+  {
+    "revision": "b7f68e8afaec5bfad8fdcc44e4e4e483",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/background_1920-11.b7f68e8a.jpg"
+  },
+  {
+    "revision": "acfe7046f47d96b9cb598622e50fe610",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/background_1920-18.acfe7046.jpg"
+  },
+  {
+    "revision": "1420d15b22ed3f39abc468212d735814",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/background_640-1.1420d15b.jpg"
+  },
+  {
+    "revision": "55e47e98ef3c84cf5eb4706a4c74311a",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/background_640-3.55e47e98.jpg"
+  },
+  {
+    "revision": "1d98ff2e44d70e391ec7a49e4efdfa86",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/background_1920-5.1d98ff2e.jpg"
+  },
+  {
+    "revision": "1e0eb5503400ca7f2a65175cec9e9115",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/background_1920-6.1e0eb550.jpg"
+  },
+  {
+    "revision": "abf47fc19906e17185a37765e7594ee0",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/background_1920-7.abf47fc1.png"
+  },
+  {
+    "revision": "22fc19f2c02f7dba20864f097f0bfd20",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/background_1920-19.22fc19f2.jpg"
+  },
+  {
+    "revision": "a9a0ec6482226ea8098fc71a3fababe0",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/product_640-1.a9a0ec64.jpg"
+  },
+  {
+    "revision": "da0b0457353c6555a5ba4535597f2187",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/product_640-2.da0b0457.jpg"
+  },
+  {
+    "revision": "64ed27c9aca0306b4185b82e144cdedd",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/product_640-3.64ed27c9.jpg"
+  },
+  {
+    "revision": "2110d8cd7d70f6792b4c2d517cb7f593",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/product_640-4.2110d8cd.jpg"
+  },
+  {
+    "revision": "bf8840bd88a80c14418b238a7f1beb24",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/product_640-5.bf8840bd.jpg"
+  },
+  {
+    "revision": "f9249755162af20121af258ca1ffc578",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/product_640-6.f9249755.jpg"
+  },
+  {
+    "revision": "de88253696b81598494dd6bd5c0b087e",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_6.de882536.jpg"
+  },
+  {
+    "revision": "1dce100ffdb944226acf9db666a57319",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_7.1dce100f.jpg"
+  },
+  {
+    "revision": "a4be0bbf3a08f9de24548aa6765af93f",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_8.a4be0bbf.jpg"
+  },
+  {
+    "revision": "798c02eb53d02e11c3d8a6588cdf5033",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_9.798c02eb.jpg"
+  },
+  {
+    "revision": "56d054ffdf15ac516d7ba19fa9a33ad5",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_10.56d054ff.jpg"
+  },
+  {
+    "revision": "336faadc989ea15bbe2c0fb30e20bcd7",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_11.336faadc.jpg"
+  },
+  {
+    "revision": "1e0889afa5d1fa653fef4f8b5d3272d2",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_12.1e0889af.jpg"
+  },
+  {
+    "revision": "63265e929c9f7442ea1853d2dc19a188",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_13.63265e92.jpg"
+  },
+  {
+    "revision": "595b8cbdd2b7f27cf7d38b895a9b56c9",
+    "url": "https://waleedjunaid.io/Smart-Care/static/media/100_14.595b8cbd.jpg"
+  },
+  {
+    "revision": "051c5a543533717a822adb18f5351f04",
+    "url": "https://waleedjunaid.io/Smart-Care/index.html"
+  }
+];
